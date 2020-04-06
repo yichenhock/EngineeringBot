cd757e0c1f9eb7fb20af0f672193f7a9580c5026
Hamezii <36954453+Hamezii@users.noreply.github.com>
2020-04-06 17:47:23 +0100

    Add files via upload

---
+++ question.py
@@ -0,0 +1,24 @@
+import random
+
+class Question:
+    answer_symbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+    def __init__(self, question_text, *answers:list, question_img=None):
+        """
+        question_text: a string
+        answers: a list of strings
+        question_img: a string
+        """
+        self.question_text = question_text
+        self.question_img = question_img
+        random.shuffle(answers)
+        self.answers = answers
+    
+    def show_question(self):
+        """Call this function to display the question"""
+        # Show question text
+        if question_img is not None:
+            pass # Show image
+        
+        for i, answer in answers:
+            answer_text = "{}. {}".format(Question.answer_symbols[i], answer)
+            # Show answer text
