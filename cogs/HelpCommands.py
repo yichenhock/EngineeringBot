d45a693addf3e0c57cb115a37e7b6b55d647e9a7
Yi Chen Hock <yichenhock@gmail.com>
2020-04-09 13:05:15 +0100

    trying to work out menu reacts

--- cogs/HelpCommands.py
+++ cogs/HelpCommands.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import discord
 
 prefix = "dad "
+left='◀️'
+right='▶️'
 
 class HelpCommands(commands.Cog):
     def __init__(self,bot):
@@ -44,7 +46,10 @@ class HelpCommands(commands.Cog):
                     halp = discord.Embed(title='Bruh!',description='How do you even use "'+cog[0]+'"?',color=discord.Color.red())
                 else:
                     await ctx.message.add_reaction(emoji='✉')
-                await ctx.message.author.send('',embed=halp)
+                msg = await ctx.message.author.send('',embed=halp)
+                await msg.add_reaction("◀️")
+                await msg.add_reaction("▶️")
+                await msg.remove_reaction(left)
         except:
             await ctx.send("Excuse me, I can't send embeds.")
         
