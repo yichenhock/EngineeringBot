b37a8f34dbfe141735d4a485701ad6312e235509
Yi Chen Hock <yichenhock@gmail.com>
2020-04-09 13:06:48 +0100

    fix!

--- cogs/HelpCommands.py
+++ cogs/HelpCommands.py
@@ -27,7 +27,7 @@ class HelpCommands(commands.Cog):
                         cmds_desc += ('{} - {}'.format(y.name,y.help)+'\n')
                 halp.add_field(name='General Commands',value=cmds_desc[0:len(cmds_desc)-1],inline=False)
                 await ctx.message.add_reaction(emoji='✉')
-                await ctx.message.author.send('',embed=halp)
+                msg = await ctx.message.author.send('',embed=halp)
                 await msg.add_reaction("◀️")
                 await msg.add_reaction("▶️")
                 await msg.remove_reaction(left)
