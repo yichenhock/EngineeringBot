4179c9ec3990f5ed484054920b00db2cb94e4b45
Yi Chen Hock <yichenhock@gmail.com>
2020-09-12 20:19:27 +0100

    minor change to role adder

--- cogs/RoleCommands.py
+++ cogs/RoleCommands.py
@@ -35,36 +35,6 @@ colleges = {
     "trinity hall" : ["tit hall"],
     "wolfson" : []
 }
-colleges1 = {
-    "christ's": [754331432335442010,["christs"]], 
-    "churchill": [754331585980923945],
-    "clare" : [754331618553888868],
-    "clare hall" : [754331638183493742],
-    "corpus" : [754331674762018849,["corpus christi"]],
-    "darwin" : [754331715081863178],
-    "downing" : [754331739928789064],
-    "emma" : [754331776318570498, ["emmanuel"]],
-    "fitz" : [754331800276434964, ["fitzwilliam"]],
-    "girton" : [754331825400184856],
-    "homerton" : [754331849693724793],
-    "hughes hall" : [754331869218209847, ["hughes"]],
-    "jesus" : [754331902676041757],
-    "lucy cav" : [754331930077429872, ["lucy","lucy cavendish"]],
-    "magdalene" : [754331949803503623,["mag"]],
-    "medwards" : [754331983332507768, ["murry edwards"]],
-    "pembroke" : [754332013749862423, ["pem"]],
-    "peterhouse" : [754332040677031976],
-    "queens'" : [754332061996941382, ["queens"]],
-    "robinson" : [754332095467356321],
-    "selwyn" : [754332117575663746],
-    "sidney sussex" : [754332137192292392,["sidney"]],
-    "catz" : [754332166548095066,["st catz", "st catherine", "st catherine's"]],
-    "st ed's" : [754332204229984346,["st edmunds", "st edmund's", "eddies", "st eds"]],
-    "st john's" : [754332241550901318,["johns","john's","st johns"]],
-    "trinity" : [754332265948905562],
-    "trinity hall" : [754332291827761232,["tit hall"]],
-    "wolfson" : [754332313076236288]
-}
 
 class RoleCommands(commands.Cog, name="Role"):
     def __init__(self,bot):
@@ -86,16 +56,20 @@ class RoleCommands(commands.Cog, name="Role"):
                         role_name = k
 
                 if role_name != "": 
-                    
+                    role_existing = False
                     role = get(message.guild.roles, name=role_name)
 
                     for r in message.author.roles: 
                         if r.name in colleges: 
-                            await message.author.remove_roles(r)
-                            await message.channel.send("You have been removed from **"+ r.name + "**.")
-
-                    await message.author.add_roles(role)
-                    await message.channel.send("You have been added to **"+ role_name + "**.")
+                            if role_name != r.name: 
+                                await message.author.remove_roles(r)
+                                await message.channel.send("You have been removed from **"+ r.name + "**.")
+                            else: 
+                                await message.channel.send("You have already been assigned to **"+ r.name + "**.")
+                                role_existing = True
+                    if not role_existing: 
+                        await message.author.add_roles(role)
+                        await message.channel.send("You have been added to **"+ role_name + "**.")
                     # await message.author.add_roles(role)
 
                 # print(message.content)
